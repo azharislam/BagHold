88d3c7008c40ad6eedf116968aa1fca9a1a95a30
Azhar Islam <azhartislam@gmail.com>
2022-05-13 16:37:55 +0100

    Initial Commit

---
+++ BagHold/BagHoldApp.swift
@@ -0,0 +1,17 @@
+//
+//  BagHoldApp.swift
+//  BagHold
+//
+//  Created by Azhar Islam on 13/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct BagHoldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
